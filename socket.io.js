1f46fe6647dc0c39bae5ae488832911635dc92a3
Isabel Paredes <isabel.paredes@quantstack.net>
2023-06-09 15:48:58 +0200

    Fix export of io

--- socket.io.js
+++ socket.io.js
@@ -1,5 +1,5 @@
 /*! Socket.IO.js build:0.9.17, development. Copyright(c) 2011 LearnBoost <dev@learnboost.com> MIT Licensed */
-var io = {};
+export var io = {};
 //var io = {};
 (function () {
     /**
