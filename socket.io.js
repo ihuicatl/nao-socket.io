0a09ff832afd0d85114536c03222ec801ecf8cd8
Isabel Paredes <isabel.paredes@quantstack.net>
2023-06-16 13:49:07 +0200

    Revert module

--- socket.io.js
+++ socket.io.js
@@ -1,6 +1,6 @@
 /*! Socket.IO.js build:0.9.17, development. Copyright(c) 2011 LearnBoost <dev@learnboost.com> MIT Licensed */
 export var io = {};
-//var io = {};
+// var io = {};
 (function () {
     /**
      * socket.io
